656b7ba73c55b27340c0a4d99cde59cf8fddbf16
SBickram <stevebickram@gmail.com>
2015-04-08 14:48:58 -0400

    inputting of backgrounds and sag animation

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$('.nav').localScroll();
+var $btnCollapse = $('.btn-collapse-expand')
+var $box = $('.box');
+
+$btnCollapse.on('click', function () {
+    $box.toggleClass('js-collapse');    
+});;
+
+var $intro = $('.intro');
+
+//$intro.waypoint(function () {
+//    $intro.addClass('js-intro-animate');
+//}, { offset: '50%'});
+//
+//var $gallery = $('.gallery');
+//
+//$gallery.waypoint(function () {
+//    $gallery.addClass('js-gallery-animate');
+//}, { offset: '50%'});
+//
+//var $contact = $('.contact');
+//
+//$contact.waypoint(function () {
+//    $contact.addClass('js-contact-animate');
+//}, { offset: '50%'});
+//
+//var $btnShowHide = $('.btn-show-hide');
+//var $contact = $('.contact');
+//
+//$btnShowHide.on('click', function () {
+//   $contact.toggleClass('js-contact-show');
+//    
+//});
+//
+//var $btnBounce = $('.btn-bounce');
+//var $ministeve = $('.mini-steve');
+//
+//$btnBounce.on('click', function () {
+//$ministeve.addClass('js-steve-animate');
+//
+//});
+//
+//var $btnfly = $('.btn-fly');
+//var $crow-fly = $('.crow-1');
+//
+//$btnfly.on('click', function () {
+//$crow-fly.addClass('js-crow-fly');
+//
+//});
