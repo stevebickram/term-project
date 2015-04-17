0cd76a40b69e3be223cdd83b22d0fd4a44b5dbe2
SBickram <stevebickram@gmail.com>
2015-04-17 00:19:28 -0400

    inputting of navigation gris, gallery js elements
    inputting of the js elements for gallery viewing and the grid for
    navigation. Also inputting of js for ghosts movement

--- js/main.js
+++ js/main.js
@@ -1,36 +1,72 @@
 $('.nav').localScroll();
 var $btnCollapse = $('.btn-collapse-expand')
 var $box = $('.box');
-
 $btnCollapse.on('click', function () {
     $box.toggleClass('js-collapse');    
 });;
 
 var $intro = $('.intro');
-
 var $ghostSection = $('.ghost');
 $ghostSection.waypoint(function () {
     $ghostSection.addClass('js-ghost-active');
-}, {offset: '65%'});
-
+    }, {offset: '65%'});
 $ghostSection.waypoint(function () {
     $ghostSection.addClass('js-mouth-active');
-}, {offset: '85%'});
+    }, {offset: '85%'});
+
+var $btnBounce1 = $('.btn-gallery-1');
+var $ghost2 = $('.ghost2');
+$btnBounce1.on('click', function () {
+$ghost2.addClass('js-ghost2');
+    });
+$ghost2.on('webkitanimationend animationend', function () {
+$ghost2.removeClass('js-ghost2');    
+    });
+var $btnGallery1 = $('.btn-gallery-1');
+var $images = $('.images');
+$btnGallery1.on('click', function () {
+$images.addClass('js-btn-gallery1');
+$images.removeClass('js-btn-gallery2');
+$images.removeClass('js-btn-gallery3');
+});
 
 var $btnBounce = $('.btn-gallery-2');
 var $ghost = $('.ghost');
-
 $btnBounce.on('click', function () {
 $ghost.addClass('js-ghost');
+    });
+$ghost.on('webkitanimationend animationend', function () {
+$ghost.removeClass('js-ghost');    
+    });
 
+var $btnGallery2 = $('.btn-gallery-2');
+$btnGallery2.on('click', function () {
+$images.addClass('js-btn-gallery2');
+$images.removeClass('js-btn-gallery1');
+$images.removeClass('js-btn-gallery3');
 });
 
-$ghost.on('webkitanimationend animationend', function () {
-    $ghost.removeClass('js-ghost');
 
-    
-  });
 
+
+var $btnGallery3 = $('.btn-gallery-3');
+$btnGallery3.on('click', function () {
+$images.addClass('js-btn-gallery3');
+$images.removeClass('js-btn-gallery1');
+$images.removeClass('js-btn-gallery2');
+});
+
+$btn-gallery2.on('click', function () {
+$images.addClass('js-btn-gallery2');
+$images.removeClass('js-btn-gallery1');
+$images.removeClass('js-btn-gallery3');
+});
+
+$btn-gallery1.on('click', function () {
+$images.addClass('js-btn-gallery1');
+$images.removeClass('js-btn-gallery2');
+$images.removeClass('js-btn-gallery3');
+});
 //$intro.waypoint(function () {
 //    $intro.addClass('js-intro-animate');
 //}, { offset: '50%'});
