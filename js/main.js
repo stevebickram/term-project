a89c3851eff0152d7bebb1c09fdd19755f4e2dd5
SBickram <stevebickram@gmail.com>
2015-04-22 00:41:03 -0400

    inputting of background s and js. components.
    inputting, sizing, placement of some js. components.

--- js/main.js
+++ js/main.js
@@ -46,9 +46,6 @@ $images.removeClass('js-btn-gallery1');
 $images.removeClass('js-btn-gallery3');
 });
 
-
-
-
 var $btnGallery3 = $('.btn-gallery-3');
 $btnGallery3.on('click', function () {
 $images.addClass('js-btn-gallery3');
@@ -67,42 +64,7 @@ $images.addClass('js-btn-gallery1');
 $images.removeClass('js-btn-gallery2');
 $images.removeClass('js-btn-gallery3');
 });
-//$intro.waypoint(function () {
-//    $intro.addClass('js-intro-animate');
-//}, { offset: '50%'});
-//
-//var $gallery = $('.gallery');
-//
-//$gallery.waypoint(function () {
-//    $gallery.addClass('js-gallery-animate');
-//}, { offset: '50%'});
-//
-//var $contact = $('.contact');
-//
-//$contact.waypoint(function () {
-//    $contact.addClass('js-contact-animate');
-//}, { offset: '50%'});
-//
-//var $btnShowHide = $('.btn-show-hide');
-//var $contact = $('.contact');
-//
-//$btnShowHide.on('click', function () {
-//   $contact.toggleClass('js-contact-show');
-//    
-//});
-//
-//var $btnBounce = $('.btn-bounce');
-//var $ministeve = $('.mini-steve');
-//
-//$btnBounce.on('click', function () {
-//$ministeve.addClass('js-steve-animate');
-//
-//});
-//
-//var $btnfly = $('.btn-fly');
-//var $crow-fly = $('.crow-1');
-//
-//$btnfly.on('click', function () {
-//$crow-fly.addClass('js-crow-fly');
-//
-//});
+
+$intro.waypoint(function () {
+    $intro.addClass('js-intro-animate');
+}, { offset: '50%'});
